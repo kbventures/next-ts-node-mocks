099c226d00fd25e38141d99d6cee8c33e71921be
Ken Beaudin <30326135+kbventures@users.noreply.github.com>
2023-11-10 11:27:51 -0500

    Update example.test.ts

--- __tests__/example.test.ts
+++ __tests__/example.test.ts
@@ -1,20 +1,23 @@
-// __tests__/example.test.ts
-import { NextApiRequest, NextApiResponse } from 'next';
+import httpMocks from 'node-mocks-http';
 import exampleApi from '../pages/api/example';
 
 jest.mock('next');
 
 describe('/api/example', () => {
   it('should return a message', async () => {
-    const req = {} as NextApiRequest;
-    const res = {
-      status: jest.fn(() => res),
-      json: jest.fn(),
-    } as unknown as NextApiResponse;
+    // Create a mock request and response using node-mocks-http
+    const req = httpMocks.createRequest({
+      method: 'GET',
+      url: '/api/example',
+    });
 
+    const res = httpMocks.createResponse();
+
+    // Call the exampleApi with the mock request and response
     await exampleApi(req, res);
 
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith({ message: 'Hello from the API!' });
+    // Use node-mocks-http assertions for the response
+    expect(res._getStatusCode()).toBe(200);
+    expect(JSON.parse(res._getData())).toEqual({ message: 'Hello from the API!' });
   });
 });
